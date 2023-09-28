00961261498379c7cfb45fa366b205bda598cf34
musebe <emusebe9@gmail.com>
2023-09-28 12:45:41 +0300

    Navbar typescript adjustment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,17 @@
 'use client';
-import React, { useState } from 'react';
+
+import React, { FC, useState } from 'react';
 import Link from 'next/link';
 import { Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid';
 
-const Navbar = () => {
-  const Links = [
+type LinkType = {
+  name: string;
+  link: string;
+  className: string;
+};
+
+const Navbar: FC = () => {
+  const links: LinkType[] = [
     {
       name: 'About Us',
       link: '/about',
@@ -16,16 +23,17 @@ const Navbar = () => {
       className: 'body-text !font-normal',
     },
   ];
-  const [open, setOpen] = useState(false);
 
-  const handleLinkClick = () => {
-    setOpen(false);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const toggleMenu = (): void => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
     <div className='shadow-md w-full fixed top-0 left-0 z-50'>
-      <div className='md:flex items-center justify-between bg-black-100 py-4 md:px-10 px-7 text-white'>
-        {/* logo section */}
+      <div className='md:flex items-center justify-between py-7 md:px-10 px-7 bg-black-100 text-white'>
+        {/* Logo section */}
         <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
           <Link
             href='/'
@@ -37,30 +45,26 @@ const Navbar = () => {
 
         {/* Menu icon */}
         <div
-          onClick={() => setOpen(!open)}
-          className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7'
+          onClick={toggleMenu}
+          className='absolute right-8 top-1/2 transform -translate-y-1/2 cursor-pointer md:hidden w-7 h-7'
         >
-          {open ? (
+          {isMenuOpen ? (
             <XMarkIcon className='text-white' />
           ) : (
             <Bars3BottomRightIcon className='text-white' />
           )}
         </div>
 
-        {/* link items */}
+        {/* Link items */}
         <ul
           className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-black-100 md:z-auto z-40 left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? 'top-12' : 'top-[-490px]'
+            isMenuOpen ? 'top-12' : 'top-[-490px]'
           }`}
         >
-          {Links.map((link) => (
-            <li className='md:ml-8 md:my-0 my-7' key={link.name}>
-              <Link
-                href={link.link}
-                className={link.className}
-                onClick={handleLinkClick}
-              >
-                {link.name}
+          {links.map(({ name, link, className }) => (
+            <li key={name} className='md:ml-8 md:my-0 my-7'>
+              <Link href={link} className={className} onClick={toggleMenu}>
+                {name}
               </Link>
             </li>
           ))}
@@ -71,4 +75,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
